4f8fcff0e4cc51b163f8170d087ab003203bad35
Kuanysh <developerandroidkz@gmail.com>
2024-01-19 02:20:12 +0500

    feat: sign up route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('{locale}')
+    ->where(['locale' => '[a-zA-Z]{2}'])
+    ->middleware('locale')
+    ->group(function () {
+
+        Route::post('signUp', [AuthController::class, 'signUp'])->name('signUp');
+
+    });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
