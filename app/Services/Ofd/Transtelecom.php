b53f2f73d93e97d6283bce5e13469d73f7bb26be
Kuanysh <developerandroidkz@gmail.com>
2024-01-22 22:01:32 +0500

    feat: trans telecom parsing

---
+++ app/Services/Ofd/Transtelecom.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services\Ofd;
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Transtelecom
+{
+
+    public static $client;
+
+    public static string $url;
+
+    /**
+     * @param  string  $url
+     */
+    public function __construct($url)
+    {
+        self::$url = $url;
+        self::$client = new Client([
+            'allow_redirects' => false,
+        ]);
+    }
+
+    /**
+     *
+     * @param  string $url
+     *
+     * @return static
+     */
+    public static function make(string $url = '')
+    {
+        return new static($url);
+    }
+
+    public static function positions(): array
+    {
+        $positions = [];
+        $html = self::$client->get(self::$url)->getBody()->getContents();
+        $crawler = new Crawler($html);
+        $crawler->filter('ol.ready_ticket__items_list > li > span')->each(function (Crawler $crawler) use(&$positions) {
+            $positions[] = str($crawler->html())->squish()->lower()->value();
+        });
+        return $positions;
+    }
+}
--- config/settings.php
+++ config/settings.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Services\Ofd\Kazakhtelecom;
+use App\Services\Ofd\Transtelecom;
 
 return [
     'ofd_domains' => [
-        'ofd1.kz' => '1',
+        'ofd1.kz' => Transtelecom::class,
         'consumer.oofd.kz' => Kazakhtelecom::class,
         'ofd.soliq.uz' => '3',
     ],
