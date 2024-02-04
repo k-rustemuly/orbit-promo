71d16c7fec8c78b4f96e903de95d5ea6554b366c
Kuanysh <developerandroidkz@gmail.com>
2024-02-04 22:40:28 +0500

    fix:bug

--- app/MoonShine/Pages/RegistrationPage.php
+++ app/MoonShine/Pages/RegistrationPage.php
@@ -27,28 +27,44 @@ class RegistrationPage extends Page
 
     public function components(): array
 	{
+        $settings = app(GeneralSettings::class);
+        $startTime = $settings->start_date;
+        $endTime = $settings->end_date;
+        $nextMonday = $startTime->copy()->startOfWeek(Carbon::MONDAY);
+        $allMondays = [];
+        while ($nextMonday->lte($endTime)) {
+            $allMondays[$nextMonday->format('d.m.Y')] = 0;
+            $nextMonday->addWeek();
+        }
 		return [
             LineChartMetric::make(__('ui.messages.users'))
                 ->withoutSortKeys()
                 ->line([
-                    __('ui.messages.registration_users') => User::selectRaw('WEEK(created_at, 1) as week, COUNT(*) as count')
-                        ->groupBy(DB::raw('WEEK(created_at, 1)'))
-                        ->get()
-                        ->mapWithKeys(function ($item) {
-                            return [
-                                \Carbon\Carbon::now()->startOfYear()->addWeeks($item->week - 1)->format('d.m.Y') => $item->count
-                            ];
-                        })
+                    __('ui.messages.registration_users') =>
+                        collect($allMondays)
+                        ->merge(
+                            User::selectRaw('WEEK(created_at, 1) as week, COUNT(*) as count')
+                                ->groupBy(DB::raw('WEEK(created_at, 1)'))
+                                ->get()
+                                ->mapWithKeys(function ($item) {
+                                    return [
+                                        \Carbon\Carbon::now()->startOfYear()->addWeeks($item->week - 1)->format('d.m.Y') => $item->count
+                                    ];
+                                })
+                        )
                         ->toArray(),
-                    __('ui.messages.not_verified_users') => User::selectRaw('WEEK(created_at, 1) as week, COUNT(*) as count')
-                        ->whereNull('phone_number_verified_at')
-                        ->groupBy(DB::raw('WEEK(created_at, 1)'))
-                        ->get()
-                        ->mapWithKeys(function ($item) {
-                            return [
-                                \Carbon\Carbon::now()->startOfYear()->addWeeks($item->week -1)->format('d.m.Y') => $item->count
-                            ];
-                        })
+                    __('ui.messages.not_verified_users') =>
+                        collect($allMondays)
+                            ->merge(User::selectRaw('WEEK(created_at, 1) as week, COUNT(*) as count')
+                                    ->whereNull('phone_number_verified_at')
+                                    ->groupBy(DB::raw('WEEK(created_at, 1)'))
+                                    ->get()
+                                    ->mapWithKeys(function ($item) {
+                                        return [
+                                            \Carbon\Carbon::now()->startOfYear()->addWeeks($item->week -1)->format('d.m.Y') => $item->count
+                                        ];
+                                    })
+                            )
                         ->toArray(),
                 ],[
                     'red', 'blue'
