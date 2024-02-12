4c90d8a850a401cf82dbd00516ee510ded743c4c
Kuanysh <developerandroidkz@gmail.com>
2024-02-12 23:43:38 +0500

    fix: winned date for winned vouchers

--- app/Http/Resources/VouchersResource.php
+++ app/Http/Resources/VouchersResource.php
@@ -15,7 +15,7 @@ class VouchersResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'date' => $this->created_at->format('d.m.Y'),
+            'date' => $this->is_approved && ! is_null($this->winned_date) ? $this->winned_date->format('d.m.y') : $this->created_at->format('d.m.Y'),
             'phone_number' => $this->user->hidden_phone_number,
             'spent_bal' =>  $this->spent_balls,
             'prize' => $this->prize->name,
